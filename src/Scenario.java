2d773a5cc6efa4b449e8b24924749db4aaccd444
prudhvi <chandupinnamani@gmail.com>
2021-05-14 16:36:24 +0530

    exmaple for class and object

---
+++ src/Scenario.java
@@ -0,0 +1,14 @@
+public class Scenario {
+
+    public static void main(String[] args) {
+
+
+        Mobile kushvanthMobile=new Mobile(12000,6,64);
+        Mobile prudhviMobile=new Mobile(1200,4,32);
+        Mobile hanumanthMobile=new Mobile(82,6,128);
+        kushvanthMobile.getInfo();
+        hanumanthMobile.getInfo();
+        prudhviMobile.getInfo();
+
+    }
+}
